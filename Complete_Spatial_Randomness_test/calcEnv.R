b9cee91a41310b58f0d49af17794c9f94c721f37
NMDimitriou <42852563+NMDimitriou@users.noreply.github.com>
2021-01-21 09:07:57 -0500

    Add files via upload

--- Complete_Spatial_Randomness_test/calcEnv.R
+++ Complete_Spatial_Randomness_test/calcEnv.R
@@ -1,16 +1,21 @@
+library(foreach)
+library(doParallel)
+library(spatstat.data)
+library(nlme)
+library(rpart)
 library(spatstat)
-library(doParallel) 
+
 calcEnv <- function(A,fname){
-  
-  boxr    <- box3(xrange = c(1,2484),yrange = c(1,2484),zrange = c(1,900),unitname = "microns") #1,max(A[,3])
+
+  boxr    <- box3(xrange = c(1,2500),yrange = c(1,2500),zrange = c(1,917),unitname = "microns") #1,max(A[,3])
   coorA   <- pp3(A$V1,A$V2,A$V3,boxr,marks = NULL)
-  #K       <- K3est(coorA,correction = "translation")
-  calcEnv <- envelope.pp3(coorA, K3est, correction = "translation", nsim=99, nrank=1)#, transform = expression(. -4*pi*r^3/3))
-  write.csv(calcEnv,paste("resEnv_K_original/Kenv",fname,"csv",sep = "."),row.names = FALSE)
+  calcEnv <- envelope.pp3(coorA, K3est, correction = c("isotropic","translation"), nsim=99, nrank=1)#, transform = expression(. -4*pi*r^3/3))
+  write.csv(calcEnv,paste("../resEnv_sim_comp/K_env/Kenv_raw_series_1",fname,"csv",sep = "."),row.names = FALSE)
   return(fname)
 }
 
-dat.files     <- list.files(path="res_coord_scaled",recursive=T,pattern="*.txt",full.names=T) # Must be scaled coordinates!!!
+# Series 1
+dat.files     <- list.files(path="../res_coord_scaled",recursive=T,pattern="*.txt",full.names=T) # Must be scaled coordinates!!!
 coord         <- lapply(dat.files, read.delim, sep=",", header=FALSE)
 names(coord)  <- dat.files
 
@@ -22,13 +27,17 @@ registerDoParallel(cores=ncores) # Shows the number of Parallel Workers to be us
 print(ncores)                    # this how many cores are available, and how many you have requested.
 getDoParWorkers()                # you can compare with the number of actual workers
 
+nsamples <- 42
+
 # be careful! foreach() and %dopar% must be on the same line!
-foreach(i=1:42) %dopar% {
+foreach(i=1:nsamples, .verbose=T) %dopar% {
   A<-dat.files[i]
   fname<-basename(A)
+  fname
   dat<-coord[[A]]
   calcEnv(dat,fname)
 }
+
 #################### END #################################################
 ##########################################################################
 
